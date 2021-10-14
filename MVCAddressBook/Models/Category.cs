faf40d61c01e1c28885ff6f2b73792cab1d771d5
Matt Lane <84758670+LaneTrain47@users.noreply.github.com>
2021-10-14 01:37:43 -0400

    20211014_013600
    Through 1:37:00 mark of "2021_10_13 (PM)"

---
+++ MVCAddressBook/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAddressBook.Models
+{
+    public class Category
+    {
+        //Primary key
+        public int Id { get; set; }
+
+        //Foreign key
+        public string UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual AppUser User { get; set; }
+
+        public ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+
+    }
+}
